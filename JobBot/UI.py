ac64e8302840c2e2d9beb7e2fddca5700881e7b2
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-01-19 01:41:01 -0500

    UI compartmentlization

--- JobBot/UI.py
+++ JobBot/UI.py
@@ -29,3 +29,60 @@ window.close()
 
 
 #input-clock-output
+
+
+
+class UI_Manager:
+    def __init__(self):
+        self.firstWindow = None
+
+
+
+
+
+
+def initFirstWindow():
+    layout = 
+    [[sg.Text("What kind of job are you interested in")],
+    sg.Text("Please Enter a Location")]
+          [sg.Input(key='-INPUT-')],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+
+    # Create the window
+    window = sg.Window('Job Bot', layout)
+
+# Display and interact with the Window using an Event Loop
+    while True:
+    event, values = window.read()
+        # See if user wants to quit or window was closed
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
+    # Output a message to the window
+     window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying Using")
+
+# Finish up by removing from the screen
+    window.close()
+
+
+
+def initializeSecondWindow():
+    layout = [[sg.Text("What kind of job are you interested in")],sg.Text("Please Enter a Location")]
+          [sg.Input(key='-INPUT-')],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+
+# Create the window
+    window = sg.Window('Job Bot', layout)
+
+# Display and interact with the Window using an Event Loop
+    while True:
+        event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    # Output a message to the window
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying Using")
+
+# Finish up by removing from the screen
+window.close()  
