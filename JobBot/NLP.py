39a30fb49d7d78399b46d175e1e72a953926ccbe
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-01-30 23:56:10 -0500

    NLP high level processing

--- JobBot/NLP.py
+++ JobBot/NLP.py
@@ -1,11 +1,19 @@
 
-
-
+import parse
+import patterns
+import indeedBot
+from parse import Words_Phrases
+from patterns import Pattern_Manager
 
 class NLP_Manager:
 
     def __init__(self):
-        pass
+        self.words_phrases = Words_Phrases()
+        self.pattern_manager = Pattern_Manager()
+
+        pattern_manager.VP
+
+
 
 # this file will act as the modularized object that will acess and control all NLP functions 
 # and tasks on a high level for the overall project
