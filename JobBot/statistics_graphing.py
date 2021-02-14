fc4acbda3a8fa7388c178309d0f8c7e4314c9be3
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-02-14 00:29:00 -0500

    output/UI

--- JobBot/statistics_graphing.py
+++ JobBot/statistics_graphing.py
@@ -33,23 +33,23 @@ plt.show()
 
 
 
-#     nlp = spacy.load('en')
-#     convention_df = st.SampleCorpora.ConventionData2012.get_data(
-#     ).assign(
-#     parse=lambda df: df.text.apply(nlp),
-#     party=lambda df: df.party.apply(
-#         {#'democrat': #'Democratic', 
-#          #'republican': '#Republican'}.get
-#     )
-# )
-# corpus = st.CorpusFromParsedDocuments(
-#     convention_df,
-#     category_col='party',
-#     parsed_col='parse',
-#     feats_from_spacy_doc=st.PyTextRankPhrases()
-# ).build(
-# ).compact(
-#     st.AssociationCompactor(2000, use_non_text_features=True)
+    nlp = spacy.load('en')
+    convention_df = st.SampleCorpora.ConventionData2012.get_data(
+    ).assign(
+    parse=lambda df: df.text.apply(nlp),
+    party=lambda df: df.party.apply(
+        {#'democrat': #'Democratic', 
+         #'republican': '#Republican'}.get
+    )
+)
+corpus = st.CorpusFromParsedDocuments(
+    convention_df,
+    category_col='party',
+    parsed_col='parse',
+    feats_from_spacy_doc=st.PyTextRankPhrases()
+).build(
+).compact(
+    st.AssociationCompactor(2000, use_non_text_features=True)
 
 class Stats_Graph_Manager:
     def __init__(self):
@@ -65,6 +65,26 @@ def graph(self):
     pass
         
 
+
+
+
+class graph:
+    def __init__(self,gdict=None):
+        if gdict is None:
+            gdict = []
+        self.gdict = gdict
+
+# Get the keys of the dictionary
+    def getVertices(self):
+        return list(self.gdict.keys())
+
+# Create the dictionary with graph elements
+graph_elements = { "a" : ["b","c"],
+                "b" : ["a", "d"],
+                "c" : ["a", "d"],
+                "d" : ["e"],
+                "e" : ["d"]
+                }
 
 
 
